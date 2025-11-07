36b6e036bdd29c49f2e1c453bbb3d86b219539ba
Kawã Felipe Ducla Aguiar <kawafelipeducla01@gmail.com>
2025-11-07 11:34:24 -0300

    .
    .

--- app.py
+++ app.py
@@ -41,4 +41,7 @@ def update(id):
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
+
+
